5638c01a2b429965774ae577c08e3076478a707e
IfycodeN <ifyugwumba22@gmail.com>
2023-03-22 16:42:08 +0100

    Added social auth

--- Accounts/models.py
+++ Accounts/models.py
@@ -1,22 +1,54 @@
 from datetime import datetime
 from django.utils import timezone
 from django.db import models
-from django.contrib.auth.models import AbstractUser
+from django.contrib.auth.models import AbstractUser, BaseUserManager
+from django.utils.translation import gettext_lazy as _
+
+class CustomUserManager(BaseUserManager):
+    """Define a model manager for User model with no username field."""
+
+    def _create_user(self, email, password=None, **extra_fields):
+        """Create and save a User with the given email and password."""
+        if not email:
+            raise ValueError('The given email must be set')
+        email = self.normalize_email(email)
+        user = self.model(email=email, **extra_fields)
+        user.set_password(password)
+        user.save(using=self._db)
+        return user
+
+    def create_user(self, email, password=None, **extra_fields):
+        extra_fields.setdefault('is_staff', False)
+        extra_fields.setdefault('is_superuser', False)
+        return self._create_user(email, password, **extra_fields)
+
+    def create_superuser(self, email, password=None, **extra_fields):
+        """Create and save a SuperUser with the given email and password."""
+        extra_fields.setdefault('is_staff', True)
+        extra_fields.setdefault('is_superuser', True)
+
+        if extra_fields.get('is_staff') is not True:
+            raise ValueError('Superuser must have is_staff=True.')
+        if extra_fields.get('is_superuser') is not True:
+            raise ValueError('Superuser must have is_superuser=True.')
+
+        return self._create_user(email, password, **extra_fields)
 
 
 class User(AbstractUser):
-    email = models.EmailField(unique=True)
+    username = None
+    email = models.EmailField(_('email address'), unique=True)
     is_email_confirmed= models.BooleanField(default=False)
 
-
     USERNAME_FIELD = 'email'
-    REQUIRED_FIELDS = ['username']
-    def __str__(self):
-        return self.email
+    REQUIRED_FIELDS = []
+
+    objects = CustomUserManager()
 
     @property
     def is_email_verified(self):
         return self.is_email_confirmed
+
 
 
 class Token(models.Model):
